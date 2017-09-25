8bf1b286afe3671d923fe4f39cf2e764a337698b
Alan Martinson <rrtx2000-git@yahoo.com>
2017-09-24 22:45:26 -0500

    functional

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
-<?php require_once 'trip_logic.php'; ?>
+<?php require_once 'trip_logic.php';?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Alan Martinson</title>
+        <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
         <link rel='stylesheet' type='text/css' href='css/main.css' />
     </head>
     <body>
         
         <?php
             #echo("<pre>Get contents:<br/>" . print_r($_GET, 1) . "</pre>");
-            $MAXIMUM_SPEED = 120;
         ?>
         
         <div id='mainBody'>
             <h1 class='mycenter'>Alan Martinson - P2 For CSCI E15</h1>
             
             <h2 id='program_title'>Trip Time Calculator</h2>
-        
+            
             <div>
                 This application calculates the estimated time a trip will take you.
                 You enter the number of miles you will drive, your anticipated estimated speed, and
@@ -25,16 +26,26 @@
                 <br/>
             </div>
             
+            <?php if (isset($errors) && !empty($errors)) :?>
+                <div class='alert alert-danger'>
+                    <ul>
+                        <?php foreach ($errors as $error) :?>
+                            <li><?=$error?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+            
             <form action='#'>
-                <label for='numberOfMiles'>Number of miles you will drive: </label>
+                <label for='numberOfMiles'>Number of miles you will drive (required): </label>
                 <input type='text' name='numberOfMiles' id='numberOfMiles' value='<?php echo($numberOfMiles)?>'>
                 
                 <br/>
-                <label for='estimatedSpeed'>Estimated Speed: </label>
+                <label for='estimatedSpeed'>Estimated Speed (required): </label>
                 <select name='estimatedSpeed' id='estimatedSpeed'>
                     <?php
                         
-                        for ($i=5; $i<=$MAXIMUM_SPEED; $i= $i+5){
+                        for ($i=$MINIMUM_SPEED; $i<=$MAXIMUM_SPEED; $i= $i+5){
                             if($i == $estimatedSpeed){
                                 $estimatedSpeedSelected = " selected";
                             }
@@ -55,24 +66,16 @@
                 //should we display the results?
                 if(($estimatedSpeed != '') && ($numberOfMiles != '')){
             ?>
+            
                 <div id='results'>
                     <h2>Results</h2>
-                    the results will go here<br/>
                     
-                    <?php
-                        if ($roundOffResults){
-                            $willOrWillNot = "";
-                        }
-                        else {
-                            $willOrWillNot = " NOT";
-                        }
-                    ?>
-                    We will <?php echo($willOrWillNot);?> be rounding off the results.<br/>
-                    Based on your expected average speed of <?php echo($estimatedSpeed);?>, it will take you
-                    x hours and y minutes to travel <?php echo($numberOfMiles);?> miles.
+                    Based on your expected average speed of <?php echo($estimatedSpeed);?>mph, it will take you
+                    <?php echo($totalHours);?> hours and <?php echo((int)$totalMinutes);?> minutes to travel <?php echo($numberOfMiles);?> miles.
                 </div>
+                
             <?php
-                //should we display the results?
+                //closing of "should we display the results?"
                 }
             ?>
             
--- trip_logic.php
+++ trip_logic.php
@@ -1,25 +1,74 @@
 <?php
-    if (isset($_GET['numberOfMiles'])){
-        $numberOfMiles = $_GET['numberOfMiles'];
+require_once('Form.php');
+//use DWA\Form;
+$form = new DWA\Form($_GET);
+#echo ($form);
+
+$MINIMUM_SPEED = 5;
+$MAXIMUM_SPEED = 120;
+        
+if ($form->isSubmitted()) {
+    $errors = $form->validate(
+        [
+            'numberOfMiles' => 'required|numeric',
+            'estimatedSpeed' => 'required|numeric|min:' . $MINIMUM_SPEED . '|max:' . $MAXIMUM_SPEED
+        ]
+    );
+}
+
+if (isset($_GET['numberOfMiles'])){
+    $numberOfMiles = $_GET['numberOfMiles'];
+}
+else {
+    $numberOfMiles = '';
+}
+
+if (isset($_GET['estimatedSpeed'])){
+    $estimatedSpeed = $_GET['estimatedSpeed'];
+}
+else {
+    //if it's not provided, start with a generic value
+    $estimatedSpeed = '60';
+}
+
+if (isset($_GET['roundOff'])){
+    $roundOffResults = true;
+    $roundOffChecked = " checked='checked'";
+}
+else {
+    $roundOffResults = false;
+    $roundOffChecked = "";
+}
+
+
+$totalHours = 0;
+$totalMinutes = 0;
+if ($form->isSubmitted()) {
+    if (empty($errors)){
+        //We have a valid request, calculate the results.
+
+        $totalHours = intdiv($numberOfMiles, $estimatedSpeed);
+        $totalMinutes = (($numberOfMiles / $estimatedSpeed) - $totalHours) * 60;        //(decimal result - totalHours) * 60 converts to minutes
+        
+        if ($roundOffResults){
+            //We want to round off to the nearest 15 minutes
+            if ($totalMinutes>45){
+                //Round up to the next hour
+                $totalMinutes = 0;
+                $totalHours++;
+            }
+            else if($totalMinutes>30){
+                $totalMinutes=45;
+            }
+            else if($totalMinutes>15){
+                $totalMinutes=30;
+            }
+            else if($totalMinutes>0){
+                $totalMinutes=15;
+            }
+        }
+        
     }
-    else {
-        $numberOfMiles = '';
-    }
-    
-    if (isset($_GET['estimatedSpeed'])){
-        $estimatedSpeed = $_GET['estimatedSpeed'];
-    }
-    else {
-        //if it's not provided, start with a generic value
-        $estimatedSpeed = '60';
-    }
-    
-    if (isset($_GET['roundOff'])){
-        $roundOffResults = true;
-        $roundOffChecked = " checked='checked'";
-    }
-    else {
-        $roundOffResults = false;
-        $roundOffChecked = "";
-    }
+}
+
 ?>
